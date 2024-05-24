13cc7ddd2bf5ce45e7374eccfee8ecf37b396ac0
Aryan Gupta <aryangupta1904@gmail.com>
2024-05-24 13:22:51 +0530

    Update SingleProduct.jsx

--- src/Components/SingleProduct.jsx
+++ src/Components/SingleProduct.jsx
@@ -1,4 +1,4 @@
-not import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { getProduct } from '../Store/ActionCreators/ProductActionCreators'
 import { useDispatch, useSelector } from 'react-redux'
 import OwlCarousel from 'react-owl-carousel';
